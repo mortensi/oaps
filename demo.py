4d8a1f7c78fc900af4ae65148c763acde231f80a
Mirko Ortensi <mirko.ortensi@gmail.com>
2023-05-10 20:52:32 +0200

    add custom dataset

--- demo.py
+++ demo.py
@@ -1,12 +1,55 @@
 from oaps import *
+import shortuuid
+import pandas as pd
+
+EPSILON = 0.3
+
+
+def create_pk():
+    shortuuid.set_alphabet('123456789abcdefghijkmnopqrstuvwxyz')
+    return shortuuid.uuid()[:5]
+
+
+def print_res(res):
+    if len(res) > 0:
+        for s in res[0]:
+            r = get_db().json().get(s, '$.sentence')
+            print(s)
+            print(r)
+
+
+def quickDemo():
+    index_document("24f2g4g2", "Pasta alla carbonara is a characteristic dish of Lazio and more particularly of Rome, prepared with popular ingredients and with an intense flavour. Spaghetti alla puttanesca is an Italian pasta dish invented in Naples in the mid-20th century and made typically with tomatoes, olive oil, olives, anchovies, chili peppers, capers, and garlic—with vermicelli or spaghetti pasta.")
+    index_document("902j20rv", "This is a technical document, it describes the SID sound chip of the Commodore 64. This post is about 8 bits computers, such as Commodore 64, ZX Spectrum and other home computers")
+
+    res = check_document("this post is about commodore", 0.3)
+    print(res)
+
+    res = check_document("Pasta alla carbonara is a characteristic dish of Lazio", 0.3)
+    print(res)
+
+
+def loadCSV():
+    cnt = 0
+    df = pd.read_csv("mortensi.csv", usecols = ['Content'])
+    for index, row in df.iterrows():
+        index_document(create_pk(), row.values[0])
+        cnt = cnt + 1
+
+    print("Indexed {} elements".format(cnt))
+
+
+def testCSV():
+    res = check_document("Pasta alla carbonara is a characteristic dish of Lazio and more particularly of Rome, prepared with popular ingredients and with an intense flavour. Spaghetti alla puttanesca is an Italian pasta dish invented in Naples in the mid-20th century and made typically with tomatoes, olive oil, olives, anchovies, chili peppers, capers, and garlic—with vermicelli or spaghetti pasta. We are a bunch of people convinced that you have to pass through difficult, or better, impossible challenges to see an idea reach the production stage and possibly provide benefits.", EPSILON)
+    print_res(res)
+
+    res = check_document("This is a technical document, it describes the SID sound chip of the Commodore 64. This post is about 8 bits computers, such as Commodore 64, ZX Spectrum and other home computers", EPSILON)
+    print_res(res)
+
 
 init()
+loadCSV()
+testCSV()
+#quickDemo()
 
-index_document("24f2g4g2", "Pasta alla carbonara is a characteristic dish of Lazio and more particularly of Rome, prepared with popular ingredients and with an intense flavour. Spaghetti alla puttanesca is an Italian pasta dish invented in Naples in the mid-20th century and made typically with tomatoes, olive oil, olives, anchovies, chili peppers, capers, and garlic—with vermicelli or spaghetti pasta.")
-index_document("902j20rv", "This is a technical document, it describes the SID sound chip of the Commodore 64. This post is about 8 bits computers, such as Commodore 64, ZX Spectrum and other home computers")
 
-res = check_document("this post is about commodore", 0.3)
-print(res)
-
-res = check_document("Pasta alla carbonara is a characteristic dish of Lazio", 0.3)
-print(res)
--- oaps.py
+++ oaps.py
@@ -5,12 +5,15 @@ from redis.commands.search.field import TextField, VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 import numpy as np
 import os
-import nltk
+import re
 
-from nltk.tokenize import sent_tokenize
 
-model = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
-
+#model = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-cos-v1')
+#MODELSIZE = 384
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v1')
+MODELSIZE = 384
+#model = SentenceTransformer('sentence-transformers/all-distilroberta-v1')
+#MODELSIZE = 768
 
 def get_db(decode=True):
     try:
@@ -23,13 +26,11 @@ def get_db(decode=True):
 
 
 def init():
-    nltk.download('punkt')
-
     indexes = get_db().execute_command("FT._LIST")
     if "oaps_idx" not in indexes:
-        index_def = IndexDefinition(prefix=["oaps:"], index_type=IndexType.JSON)
+        index_def = IndexDefinition(prefix=["oaps:seq:"], index_type=IndexType.JSON)
         schema = (  TextField("$.sentence", as_name="sentence"),
-                    VectorField("$.embedding", "HNSW", {"TYPE": "FLOAT32", "DIM": 384, "DISTANCE_METRIC": "COSINE"}, as_name="embedding"))
+                    VectorField("$.embedding", "HNSW", {"TYPE": "FLOAT32", "DIM": MODELSIZE, "DISTANCE_METRIC": "COSINE"}, as_name="embedding"))
         get_db(False).ft('oaps_idx').create_index(schema, definition=index_def)
 
 
@@ -43,24 +44,33 @@ def get_embedding_as_blob(text):
 
 def index_document(pk, text):
     seq = 0
-    for txt_sentence in sent_tokenize(text):
+    sentences = re.split("[//.|//!|//?]", text)
+    for txt_sentence in sentences:
         sentence = {
             'seq': seq,
             'sentence': txt_sentence,
             'embedding': get_embedding_as_vector(txt_sentence)
         }
         
-        get_db(False).json().set("oaps:{}:{}".format(pk,seq), '$', sentence)
+        get_db(False).json().set("oaps:seq:{}:{}".format(pk,seq), '$', sentence)
         seq = seq + 1
+    
+    get_db(False).json().set("oaps:doc:{}".format(pk), '$', text)
 
 
 def check_document(text, epsilon):
-    q = Query("@embedding:[VECTOR_RANGE $radius $vec]=>{$YIELD_DISTANCE_AS: score}")\
-        .sort_by("score", asc=True)\
-        .return_field("score")\
-        .dialect(2)
+    res = []
+    sentences = re.split("[//.|//!|//?]", text)
+    for txt_sentence in sentences:
+        q = Query("@embedding:[VECTOR_RANGE $radius $vec]=>{$YIELD_DISTANCE_AS: score}")\
+            .sort_by("score", asc=True)\
+            .return_field("score")\
+            .dialect(2)
+            
+        p = {"vec": get_embedding_as_blob(txt_sentence), "radius": epsilon}
         
-    p = {"vec": get_embedding_as_blob(text), "radius": epsilon}
-    
-    res = get_db(False).ft("oaps_idx").search(q, p).docs
+        found = get_db(False).ft("oaps_idx").search(q, p).docs
+        if len(found) > 0:
+            res.append([x['id'] for x in found])
+
     return res
